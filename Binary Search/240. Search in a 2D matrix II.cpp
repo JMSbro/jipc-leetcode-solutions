52f0c339132fb1be170cb1ac773de217f1500fd6
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:39:48 +0600

    Implement 240. binary search in a 2D matrix

--- Binary Search/240. Search in a 2D matrix II.cpp
+++ Binary Search/240. Search in a 2D matrix II.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
 
+        for(int i = 0; i<m; i++) {
+            int idx = lower_bound(matrix[i].begin(), matrix[i].end(), target) - matrix[i].begin();
+            if(idx < n && matrix[i][idx] == target)return true;
+        }
+
+        return false;
+    }
+};
