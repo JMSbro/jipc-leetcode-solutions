022b2ccd6060ce7269623d58a3dc6b11a45016da
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:47:29 +0600

    Implement 367. binary search to check perfect square

--- Binary Search/367. Valid Perfect Square.cpp
+++ Binary Search/367. Valid Perfect Square.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            long long square = (long long)mid * (long long)mid;
+            if(square == num)return true;
+            else if(square < num)low = mid + 1;
+            else high = mid - 1;
+        }
 
+        return false;
+    }
+};
